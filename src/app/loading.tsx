a347933abb95a862456166bd140516cb2f8aa5a7
haris priambada <primbodoharis@gmail.com>
2024-10-02 12:06:58 +0700

    selesai menambah loading indikator

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="grid place-items-center w-full h-screen font-bold text-sm">
+      loading....
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import Navbar from "./_components/Navbar";
 import TableData from "./_components/TableData";
 import { getData } from "./api/actions";
+import Loading from "./loading";
 
 export default async function Home() {
   const dataUniversitas = await getData();
@@ -9,7 +11,9 @@ export default async function Home() {
     <div className="mx-auto  container">
       <Navbar />
 
-      <TableData data={dataUniversitas} />
+      <Suspense fallback={<Loading />}>
+        <TableData data={dataUniversitas} />
+      </Suspense>
     </div>
   );
 }
